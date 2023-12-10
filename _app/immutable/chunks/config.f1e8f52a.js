4d1181aa43980b7cf249baf56f0c012498ef65fa
billyoyo <billyoyo@hotmail.co.uk>
2023-12-10 21:35:50 +0000

    add yzproto

---
+++ _app/immutable/chunks/config.f1e8f52a.js
@@ -0,0 +1,1 @@
+const t="yzproto",n=o=>{window.location.pathname=`/${t}${o}`};export{n as g};
---
+++ _app/immutable/nodes/3.3c93020b.js
@@ -0,0 +1,1 @@
+import{s as n,o as s}from"../chunks/scheduler.5d9df325.js";import{S as e,i as r}from"../chunks/index.cbe6f03d.js";import{g as a}from"../chunks/config.f1e8f52a.js";function i(o){return s(()=>a("/consent-form")),[]}class p extends e{constructor(t){super(),r(this,t,i,null,n,{})}}export{p as component};
